cf87b5904e27303086c0af6af9950d8b46d0bad9
9iu8igytg <akirk26_student@ivycollegiate.org>
2024-09-25 06:17:06 +0000

    test message

--- sprint_project_1.6/sprint_project_1.6.py
+++ sprint_project_1.6/sprint_project_1.6.py
@@ -1,7 +1,14 @@
 def print_menu():
 	print()
 	print()
+
 def  km_miles():
 def miles_k():
 if __name__ '__main__':
 	print_menu()
+
+
+def  km_miles():
+def miles_k():
+if __name__ '__main__':
+        print_menu()
