b25e1defd67ad31d21bd24323a65c05d54b84e64
9iu8igytg <akirk26_student@ivycollegiate.org>
2024-09-27 05:57:00 +0000

    broken code

--- sprint_project_1.6/sprint_project_1.6.py
+++ sprint_project_1.6/sprint_project_1.6.py
@@ -4,41 +4,41 @@ def print_menu():
 
 
 
-// km to miles
-def  km_miles():
-	kilometers = float ((input("Please enter a distance in Kilometers: ')))
+
+def km_miles():
+	km = float ((input('Please enter a distance in km: ')))
 	miles = km / 1.60934
 	print('Distance in miles (0)'.format(miles))
 
 
 
-// miles to km
+
 def miles_km():
 	miles = float ((input('Please enter a distance in Miles: ')))
 	km = miles * 1.60934
-	print('Distance in miles (0)'.format(miles))
+	print('Distance in miles (0)'.format(km))
 
 
 
-// cel to faren
+
 def c_to_f():
-	C = float (input('Please enter a temperature value in c '))
+	C = float ((input('Please enter a temperature value in c '))
 	F = C * (9 / 5) = 32
-	print('Tempature in f : (0).format(F)')
+	print('Tempature in f : (0)'.format(F))
 
 
 
-// faren to cel
+
 def f_to_c():
-        input('Please enter a temperature value in f '))
+	F = float (( input('Please enter a temperature value in f '))
 	C = (F * 32) * (9 / 5)
-	print('Tempature in c : (0).format(C)')
+	print('Tempature in c : (0)'.format(C))
 
 
 
 if __name__ '__main__':
         print_menu()
-	choice = input('Whcih would you like to do today?: ')
+	choice = input('Which would you like to do today?: ')
 	if choice == '1':
 	km_miles()
 	if choice == '2':
